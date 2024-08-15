fc6bd3182d3cd4c54739a58862aa045fd10d633b
jyldyzakylbekova <akylbekova@csdisco.com>
2024-08-15 14:37:12 +0600

    postpone

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Block } from './Block';
 import './index.scss';
 
 function App() {
+  const [rates, setRates] = useState({})
+
+  useEffect(()=>{
+    fetch('https://cdn.cur.su/api/latest.json')
+    .then((res)=> res.json())
+    .then((json)=>{
+      setRates(json.rates);
+      console.log(json.rates)
+    })
+    .catch((err)=>{
+      console.warn(err);
+      alert('Cannot fetch information')
+    })
+  },[]);
+
   return (
     <div className="App">
       <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
